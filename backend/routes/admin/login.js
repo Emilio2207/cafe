14b5ecce5fd346adb85dea435569ae197347d04a
EmilioRamirez <emi.emilioramirez@gmail.com>
2023-11-16 11:44:14 -0300

    m6u3: CRUD parte 3

--- backend/routes/admin/login.js
+++ backend/routes/admin/login.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var usuariosModel = require("./../../models/usuariosModel");
 
 /* GET login page. */
 router.get("/", function (req, res, next) {
@@ -8,5 +9,39 @@ router.get("/", function (req, res, next) {
     layout: "admin/layout",
   });
 });
+
+router.get("/logout", function (req, res, next) {
+  req.session.destroy(); // destruir las variables de sesion (id y usuario)
+  res.render("admin/login", {
+    layout: "admin/layout",
+  });
+}); // cierro logout
+
+/* POST login page. */
+router.post("/", async (req, res, next) => {
+  try {
+    var usuario = req.body.usuario; // captura la info 'emilio'
+    var password = req.body.password; // 123456
+
+    var data = await usuariosModel.getUserByUsernameAndPassword(
+      usuario,
+      password
+    );
+
+    if (data != undefined) {
+      req.session.id_usuario = data.id;
+      req.session.nombre = data.usuario;
+
+      res.redirect("/admin/novedades");
+    } else {
+      res.render("admin/login", {
+        layout: "admin/layout",
+        error: true,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  } // cierro catch
+}); //cierro router.post
 
 module.exports = router;
